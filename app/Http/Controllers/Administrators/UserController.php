b15166493897bedb6117c3451d14748a458b2bdf
Earl Janiel Compra <compranicles@gmail.com>
2021-07-02 12:55:34 +0800

    Delete user

--- app/Http/Controllers/Administrators/UserController.php
+++ app/Http/Controllers/Administrators/UserController.php
@@ -118,8 +118,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect->route('admin.users.index')->with('success', 'User deleted successfully');
     }
 }
