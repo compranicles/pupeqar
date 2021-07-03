c07d701b53fe401a2c8a51a92c6bab5afa025a05
Earl Janiel Compra <compranicles@gmail.com>
2021-07-03 21:56:39 +0800

    CRUD EVENT TYPE

--- app/Models/EventType.php
+++ app/Models/EventType.php
@@ -11,6 +11,7 @@ class EventType extends Model
 
     protected $fillable = [
         'name',
-        'description'
+        'description',
+        'status'
     ];
 }
--- resources/views/admin/events/types/edit.blade.php
+++ resources/views/admin/events/types/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="h4 font-weight-bold">
-            {{ __('Create Event Type') }}
+            {{ __('Update Event Type') }}
         </h2>
     </x-slot>
     <div class="container">
@@ -9,15 +9,16 @@
             <div class="col-lg-6">
                 <div class="card">
                     <div class="card-body">
-                        <form method="POST" action="{{ route('admin.event_types.store') }}">
+                        <form method="POST" action="{{ route('admin.event_types.update', $eventType->id) }}">
                             @csrf
+                            @method('put')
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="form-group">
                                         <x-jet-label value="{{ __('Event Type Name') }}" />
                     
                                         <x-jet-input class="{{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name"
-                                                    :value="old('name')" required autofocus autocomplete="name" />
+                                                    :value="old('name', $eventType->name )" required autofocus autocomplete="name" />
                                         <x-jet-input-error for="first_name"></x-jet-input-error>
                                     </div>
                                 </div>
@@ -28,7 +29,7 @@
                                         <x-jet-label value="{{ __('Description') }}" />
                     
                                         <x-jet-input class="{{ $errors->has('description') ? 'is-invalid' : '' }}" type="text" name="description"
-                                                    :value="old('description')" required autofocus autocomplete="middle_name" />
+                                                    :value="old('description', $eventType->description )" required autofocus autocomplete="middle_name" />
                                         <x-jet-input-error for="description"></x-jet-input-error>
                                     </div>
                                 </div>
@@ -36,7 +37,7 @@
                             <div class="mb-0">
                                 <div class="d-flex justify-content-end align-items-baseline">
                                     <x-jet-button>
-                                        {{ __('Create') }}
+                                        {{ __('save') }}
                                     </x-jet-button>
                                 </div>
                             </div>
