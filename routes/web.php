c3b286348adf823d7c4b81088a30bc27d7c22e1b
Earl Janiel Compra <compranicles@gmail.com>
2021-07-06 11:26:53 +0800

    Update web.php
    Bug where Logged In users can access login page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
-})->name('home');
+})->name('home')->middleware('guest');
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
