5cd788390439a1f10ab3595ec5785dd20d390bc2
Earl Janiel Compra <compranicles@gmail.com>
2021-11-11 17:08:57 +0800

    Update research-code.blade.php

--- resources/views/research/research-code.blade.php
+++ resources/views/research/research-code.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-sm modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="addModalLabel">Add Dropdown</h5>
+                <h5 class="modal-title" id="addModalLabel">Add Research</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
