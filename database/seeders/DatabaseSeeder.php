2fb7aaa78a26d233b790bd8e660676f577987667
kenpenndmnl <kenyleenp@gmail.com>
2021-11-13 13:13:00 +0800

    Seeders for consistent database 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            PermissionSeeder::class,
             FieldTypeSeeder::class,
             ResearchDropdownSeeder::class,
             ResearchFormSeeder::class,
