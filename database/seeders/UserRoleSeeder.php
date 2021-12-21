52e23d7049960fea6d750e9e84c7c603bfae454b
kenpenndmnl <kenyleenp@gmail.com>
2021-12-21 11:08:35 +0800

    seeders

--- database/seeders/UserRoleSeeder.php
+++ database/seeders/UserRoleSeeder.php
@@ -22,30 +22,42 @@ class UserRoleSeeder extends Seeder
         //Faculty user role
         UserRole::create(['user_id' => '3', 'role_id' => '1']);
         UserRole::create(['user_id' => '4', 'role_id' => '1']);
+        for ($f = 5; $f <= 15; $f++) {
+            UserRole::create(['user_id' => $f, 'role_id' => '1']);
+        }
 
         //Faculty with designation user role
-        UserRole::create(['user_id' => '5', 'role_id' => '2']);
-        UserRole::create(['user_id' => '6', 'role_id' => '2']);
+        UserRole::create(['user_id' => '16', 'role_id' => '2']);
+        UserRole::create(['user_id' => '17', 'role_id' => '2']);
+        for ($fd = 18; $fd <= 28; $fd++) {
+            UserRole::create(['user_id' => $fd, 'role_id' => '2']);
+        }
 
         //Admin employee user role
-        UserRole::create(['user_id' => '7', 'role_id' => '3']);
-        UserRole::create(['user_id' => '8', 'role_id' => '3']);
+        UserRole::create(['user_id' => '29', 'role_id' => '3']);
+        UserRole::create(['user_id' => '30', 'role_id' => '3']);
+        for ($a = 31; $a <= 41; $a++) {
+            UserRole::create(['user_id' => $a, 'role_id' => '3']);
+        }
 
         //Admin with teaching load user role
-        UserRole::create(['user_id' => '9', 'role_id' => '4']);
-        UserRole::create(['user_id' => '10', 'role_id' => '4']);
+        UserRole::create(['user_id' => '42', 'role_id' => '4']);
+        UserRole::create(['user_id' => '43', 'role_id' => '4']);
+        for ($at = 44; $at <= 54; $at++) {
+            UserRole::create(['user_id' => $at, 'role_id' => '4']);
+        }
 
         //Chairperson user role
-        UserRole::create(['user_id' => '11', 'role_id' => '5']);
-        UserRole::create(['user_id' => '12', 'role_id' => '5']);
+        UserRole::create(['user_id' => '55', 'role_id' => '5']);
+        UserRole::create(['user_id' => '56', 'role_id' => '5']);
 
         //Director/Dean user role
-        UserRole::create(['user_id' => '13', 'role_id' => '6']);
-        UserRole::create(['user_id' => '14', 'role_id' => '6']);
+        UserRole::create(['user_id' => '57', 'role_id' => '6']);
+        UserRole::create(['user_id' => '58', 'role_id' => '6']);
 
         //VP/Sector head user role
-        UserRole::create(['user_id' => '15', 'role_id' => '7']);
-        UserRole::create(['user_id' => '16', 'role_id' => '7']);
+        UserRole::create(['user_id' => '59', 'role_id' => '7']);
+        UserRole::create(['user_id' => '60', 'role_id' => '7']);
 
         //IPQMSO user role
         UserRole::create(['user_id' => '17', 'role_id' => '8']);
