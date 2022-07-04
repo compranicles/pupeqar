8879078218173d0e4132ccafbfe3081b450fe6e0
kenpenndmnl <kenyleenp@gmail.com>
2022-07-04 11:18:31 +0800

    Clear PUP Logo added
    php artisan view:clear after merging.

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a class="d-flex justify-content-center" href="/">
-    <img src="{{ URL('storage/logo2.png') }}" alt="">
+    <img src="{{ URL('storage/PUP.png') }}"  width="90" height="90" alt="PUP Logo">
 </a>
