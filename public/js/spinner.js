19db5eecc520b93e68d9d85c86150748815157f7
Earl Janiel Compra <compranicles@gmail.com>
2022-07-12 16:44:58 +0800

    Changes in spinner

--- public/js/spinner.js
+++ public/js/spinner.js
@@ -1,4 +1,13 @@
 $('form').on('submit', function() {
     $('#submit').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Saving...');
     $('#submit').attr('disabled', 'disabled');
+
+    var errorElements = document.querySelectorAll(".form-control:invalid");
+    $(errorElements[0]).focus();
+    if ($(errorElements[0]) != '') {
+        $('#submit').html('Save');
+        $('#submit').removeAttr('disabled');
+    }
 });
+
+
