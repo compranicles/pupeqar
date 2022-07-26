a6c1d4045343f8e9f0cd1c601363fa4f3a51855a
Earl Janiel Compra <compranicles@gmail.com>
2022-07-26 11:05:36 +0800

    Analytics
    Seminars and Trainings

---
+++ app/Http/Controllers/AnalyticsController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Report;
+use Illuminate\Http\Request;
+use App\Models\Maintenance\Quarter;
+use App\Http\Controllers\Controller;
+
+class AnalyticsController extends Controller
+{
+    public function index(){
+        $monthAndYear = [];
+        $years = [];
+        $quarters = [];
+
+        $currentQuarterAndYear = Quarter::find(1);
+
+        // For Seminar Hours
+        // Seminar Hours Per Month
+        $seminar_hours_per_month = [];
+
+        for($i = 0; $i < 12; $i++){
+            $seminar_hours_per_month[$i] = 0;
+            $monthAndYear[$i] = date('m-Y', strtotime('-'.$i.' months'));
+            $month = date('m', strtotime('-'.$i.' months'));
+            $year = date('Y', strtotime('-'.$i.' months'));
+
+            $seminars_per_month = Report::where('user_id', auth()->id())->
+                where('report_category_id', '25')->whereMonth('updated_at', $month)->
+                whereYear('updated_at', $year)->get();
+            foreach($seminars_per_month as $seminar){
+                $seminar_details = json_decode($seminar->report_details);
+                $seminar_hours_per_month[$i] += $seminar_details->total_hours;
+            }
+        }
+
+        // Seminar Hours Per Quarter
+        $seminar_hours_per_quarter = [];
+        $year = $currentQuarterAndYear->current_year;
+        $quarter = $currentQuarterAndYear->current_quarter;
+
+        for($i = 0; $i < 8; $i++){
+            $seminar_hours_per_quarter[$i] = 0;
+            $quarters[$i] = 'Q'.$quarter.'-'.$year;
+
+            $seminars_per_quarter = Report::where('user_id', auth()->id())->
+                where('report_category_id', '25')->where('report_quarter', $month)->
+                where('report_year', $year)->get();
+            foreach($seminars_per_quarter as $seminar){
+                $seminar_details = json_decode($seminar->report_details);
+                $seminar_hours_per_quarter[$i] += $seminar_details->total_hours;
+            }
+            $quarter = $quarter - 1;
+            if($quarter == 0){
+                $quarter = 4;
+                $year = $year - 1;
+            }
+        }
+        // Seminar Hours Per Year
+        $seminar_hours_per_year = [];
+        $year = $currentQuarterAndYear->current_year;
+
+        for($i = 0; $i < 5; $i++){
+            $seminar_hours_per_year[$i] = 0;
+            $years[$i] = $year-$i;
+
+            $seminars_per_year = Report::where('user_id', auth()->id())->
+                where('report_category_id', '25')->where('report_year', $years[$i])->get();
+
+            foreach($seminars_per_year as $seminar){
+                $seminar_details = json_decode($seminar->report_details);
+                $seminar_hours_per_year[$i] += $seminar_details->total_hours;
+            }
+
+        }
+        // --------------------------------------------------------------------------------------------------------------------
+        // For Training Hours
+        // Training Hours Per Month
+        $training_hours_per_month = [];
+
+        for($i = 0; $i < 12; $i++){
+            $training_hours_per_month[$i] = 0;
+            $monthAndYear[$i] = date('m-Y', strtotime('-'.$i.' months'));
+            $month = date('m', strtotime('-'.$i.' months'));
+            $year = date('Y', strtotime('-'.$i.' months'));
+
+            $trainings_per_month = Report::where('user_id', auth()->id())->
+                where('report_category_id', '26')->whereMonth('updated_at', $month)->
+                whereYear('updated_at', $year)->get();
+            foreach($trainings_per_month as $training){
+                $training_details = json_decode($training->report_details);
+                $training_hours_per_month[$i] += $training_details->total_hours;
+            }
+        }
+
+        // Training Hours Per Quarter
+        $training_hours_per_quarter = [];
+        $year = $currentQuarterAndYear->current_year;
+        $quarter = $currentQuarterAndYear->current_quarter;
+
+        for($i = 0; $i < 8; $i++){
+            $training_hours_per_quarter[$i] = 0;
+            $quarters[$i] = 'Q'.$quarter.'-'.$year;
+
+            $trainings_per_quarter = Report::where('user_id', auth()->id())->
+                where('report_category_id', '26')->where('report_quarter', $month)->
+                where('report_year', $year)->get();
+            foreach($trainings_per_quarter as $training){
+                $training_details = json_decode($training->report_details);
+                $training_hours_per_quarter[$i] += $training_details->total_hours;
+            }
+            $quarter = $quarter - 1;
+            if($quarter == 0){
+                $quarter = 4;
+                $year = $year - 1;
+            }
+        }
+
+        // Training Hours Per Year
+        $training_hours_per_year = [];
+        $year = $currentQuarterAndYear->current_year;
+
+        for($i = 0; $i < 5; $i++){
+            $training_hours_per_year[$i] = 0;
+            $years[$i] = $year-$i;
+
+            $trainings_per_year = Report::where('user_id', auth()->id())->
+                where('report_category_id', '26')->where('report_year', $years[$i])->get();
+
+            foreach($trainings_per_year as $training){
+                $training_details = json_decode($training->report_details);
+                $training_hours_per_year[$i] += $training_details->total_hours;
+            }
+
+        }
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        // Reverse all arrays to show most oldest first
+        $monthAndYear = array_reverse($monthAndYear);
+        $seminar_hours_per_month = array_reverse($seminar_hours_per_month);
+        $training_hours_per_month = array_reverse($training_hours_per_month);
+        $seminar_hours_per_quarter = array_reverse($seminar_hours_per_quarter);
+        $training_hours_per_quarter = array_reverse($training_hours_per_quarter);
+        $seminar_hours_per_year = array_reverse($seminar_hours_per_year);
+        $training_hours_per_year = array_reverse($training_hours_per_year);
+        $quarters = array_reverse($quarters);
+        $years = array_reverse($years);
+
+        return view('analytics.index', compact('monthAndYear', 'seminar_hours_per_month', 'seminar_hours_per_quarter', 'seminar_hours_per_year', 'training_hours_per_month', 'training_hours_per_quarter', 'training_hours_per_year', 'quarters', 'years'));
+    }
+}
---
+++ resources/views/analytics/index.blade.php
@@ -0,0 +1,189 @@
+<x-app-layout>
+    @section('title', 'Analytics |')
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="font-weight-bold mb-2">Analytics</h2>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        {{-- Seminar Card --}}
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Hours in Attending Seminars</h5>
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link " id="pills-seminar-quarter-tab" data-toggle="pill" href="#pills-seminar-quarter" role="tab" aria-controls="pills-seminar-quarter" aria-selected="false">Q</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" id="pills-seminar-year-tab" data-toggle="pill" href="#pills-seminar-year" role="tab" aria-controls="pills-seminar-year" aria-selected="true">Y</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade " id="pills-seminar-quarter" role="tabpanel" aria-labelledby="pills-seminar-quarter-tab">
+                            <div>
+                                <canvas id="seminar-quarter-chart"></canvas>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade show active" id="pills-seminar-year" role="tabpanel" aria-labelledby="pills-seminar-year-tab">
+                            <div>
+                                <canvas id="seminar-year-chart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- End Seminar Card --}}
+        {{-- Training Card --}}
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Hours in Attending Trainings</h5>
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link " id="pills-training-quarter-tab" data-toggle="pill" href="#pills-training-quarter" role="tab" aria-controls="pills-training-quarter" aria-selected="false">Q</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" id="pills-training-year-tab" data-toggle="pill" href="#pills-training-year" role="tab" aria-controls="pills-training-year" aria-selected="true">Y</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade " id="pills-training-quarter" role="tabpanel" aria-labelledby="pills-training-quarter-tab">
+                            <div>
+                                <canvas id="training-quarter-chart"></canvas>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade show active" id="pills-training-year" role="tabpanel" aria-labelledby="pills-training-year-tab">
+                            <div>
+                                <canvas id="training-year-chart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- End Training Card --}}
+    </div>
+
+    @push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    {{-- seminar year --}}
+    <script>
+        const seminarYearData = {
+            labels: JSON.parse('{!! json_encode($years) !!}'),
+            datasets: [{
+                label: 'No. of Seminar Hours per Year',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: JSON.parse('{!! json_encode($seminar_hours_per_year) !!}'),
+            }]
+        };
+
+        const seminarYearConfig = {
+            type: 'line',
+            data: seminarYearData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        const seminarYearChart = new Chart(
+            document.getElementById('seminar-year-chart'),
+            seminarYearConfig
+        );
+    </script>
+    {{-- seminar quarter --}}
+    <script>
+        const seminarQuarterData = {
+            labels: JSON.parse('{!! json_encode($quarters) !!}'),
+            datasets: [{
+                label: 'No. of Seminar Hours per Quarter',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: JSON.parse('{!! json_encode($seminar_hours_per_quarter) !!}'),
+            }]
+        };
+
+        const seminarQuarterConfig = {
+            type: 'line',
+            data: seminarQuarterData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        const seminarQuarterChart = new Chart(
+            document.getElementById('seminar-quarter-chart'),
+            seminarQuarterConfig
+        );
+    </script>
+    {{-- training year --}}
+    <script>
+        const trainingYearData = {
+            labels: JSON.parse('{!! json_encode($years) !!}'),
+            datasets: [{
+                label: 'No. of Training Hours per Year',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: JSON.parse('{!! json_encode($training_hours_per_year) !!}'),
+            }]
+        };
+
+        const trainingYearConfig = {
+            type: 'line',
+            data: trainingYearData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        const trainingYearChart = new Chart(
+            document.getElementById('training-year-chart'),
+            trainingYearConfig
+        );
+    </script>
+    {{-- training quarter --}}
+    <script>
+        const trainingQuarterData = {
+            labels: JSON.parse('{!! json_encode($quarters) !!}'),
+            datasets: [{
+                label: 'No. of Training Hours per Quarter',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: JSON.parse('{!! json_encode($training_hours_per_quarter) !!}'),
+            }]
+        };
+
+        const trainingQuarterConfig = {
+            type: 'line',
+            data: trainingQuarterData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        const trainingQuarterChart = new Chart(
+            document.getElementById('training-quarter-chart'),
+            trainingQuarterConfig
+        );
+    </script>
+    @endpush
+</x-app-layout>
