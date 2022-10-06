c0f7c378ff6e4c988b8b6d0b97949c3d5f665ee7
Ara <araalmiranez@gmail.com>
2022-10-06 11:17:08 +0800

    Hard coded Scheduling

--- app/Http/Controllers/HRISRegistration/RegistrationController.php
+++ app/Http/Controllers/HRISRegistration/RegistrationController.php
@@ -34,6 +34,41 @@ class RegistrationController extends Controller
         
         $userLocal =  User::where('email', $request->email)->first();
 
+        if ($user == '-1') {
+            
+            if (!empty($userLocal)){
+                if(!$this->scheduleCheck($userLocal->id)){
+                    return redirect()->back()->with('error', 'The college you are in is not scheduled to login today');
+                }
+                Auth::login($userLocal);
+                $user_role = UserRole::where('user_id', $userLocal->id)->whereIn('role_id', [1,3])->first();
+
+                session(['user_type' => Role::where('id', $user_role->role_id)->first()->name]);
+                if(Employee::where('user_id', $userLocal->id)->exists()){
+                    return redirect()->route('home');
+                }
+                return redirect()->route('account')->with('incomplete_account', 'incomplete_account');
+            }
+
+            $user = $this->db_ext->select(" EXEC ValidateLogIn N'$request->email',N'$request->password' ");
+
+            if($this->save($user)){
+                $userLocal = User::where('email', $request->email)->first();
+                Auth::login($userLocal);
+                $user_role = UserRole::where('user_id', $userLocal->id)->whereIn('role_id', [1,3])->first();
+                session(['user_type' => Role::where('id', $user_role->role_id)->first()->name]);
+                if(Employee::where('user_id', $userLocal->id)->exists()){
+                    return redirect()->route('home');
+                }
+                return redirect()->route('account')->with('incomplete_account', 'incomplete_account');
+            }
+        }
+
+        return redirect()->back()->with('error', 'Invalid username or password');
+    }
+
+    private function scheduleCheck($userId) {
+        
         $allowedColleges = array();
         $dateRange = ['2022-10-11','2022-10-12','2022-10-13','2022-10-14'];
         $dateToday = Carbon::today()->toDateString();
@@ -68,38 +103,11 @@ class RegistrationController extends Controller
         $endTime = Carbon::createFromFormat('H:i a', '05:00 PM');
         $timeCheck = Carbon::now()->between($startTime,$endTime,true);
 
-        if(Employee::where('user_id', $userLocal?->id)->whereIn('college_id',$allowedColleges)->doesntExist() && $timeCheck && sizeof($allowedColleges)>0){
-            return redirect()->back()->with('error', 'The college you are in is not scheduled to login today');
+        if(Employee::where('user_id', $userId)->whereIn('college_id',$allowedColleges)->doesntExist() && $timeCheck && sizeof($allowedColleges)>0){
+            return false;
+        } else {
+            return true;
         }
-
-        if ($user == '-1') {
-            
-            if (!empty($userLocal)){
-                Auth::login($userLocal);
-                $user_role = UserRole::where('user_id', $userLocal->id)->whereIn('role_id', [1,3])->first();
-
-                session(['user_type' => Role::where('id', $user_role->role_id)->first()->name]);
-                if(Employee::where('user_id', $userLocal->id)->exists()){
-                    return redirect()->route('home');
-                }
-                return redirect()->route('account')->with('incomplete_account', 'incomplete_account');
-            }
-
-            $user = $this->db_ext->select(" EXEC ValidateLogIn N'$request->email',N'$request->password' ");
-
-            if($this->save($user)){
-                $userLocal = User::where('email', $request->email)->first();
-                Auth::login($userLocal);
-                $user_role = UserRole::where('user_id', $userLocal->id)->whereIn('role_id', [1,3])->first();
-                session(['user_type' => Role::where('id', $user_role->role_id)->first()->name]);
-                if(Employee::where('user_id', $userLocal->id)->exists()){
-                    return redirect()->route('home');
-                }
-                return redirect()->route('account')->with('incomplete_account', 'incomplete_account');
-            }
-        }
-
-        return redirect()->back()->with('error', 'Invalid username or password');
     }
 
     public function create(Request $request, $key){
@@ -147,45 +155,10 @@ class RegistrationController extends Controller
     public function alternateLog(Request $request){
         $userLocal = User::where('user_account_id', $request->email)->first();
 
-        $allowedColleges = array();
-        $dateRange = ['2022-10-11','2022-10-12','2022-10-13','2022-10-14'];
-        $dateToday = Carbon::today()->toDateString();
-        
-        switch ($dateToday) {
-            case '2022-10-06':
-                array_push($allowedColleges,239,233,238,243);
-                array_push($allowedColleges,176,159,137,211,200);
-                break;
-
-            case '2022-10-07':
-                array_push($allowedColleges,274,42,40,36);
-                array_push($allowedColleges,61,66,76,83,12,25,8);
-                break;
-
-            case '2022-10-10':
-                array_push($allowedColleges,290,263,96,107,452,112);
-                array_push($allowedColleges,99,103,115,451,114);
-                break;
-
-            case in_array($dateToday,$dateRange):
-                array_push($allowedColleges,120,226,94);
-                array_push($allowedColleges,59,75,18,2);
-                break;
-            
-            default:
-                # code...
-                break;
-        }
-        
-        $startTime = Carbon::createFromFormat('H:i a', '08:00 AM');
-        $endTime = Carbon::createFromFormat('H:i a', '05:00 PM');
-        $timeCheck = Carbon::now()->between($startTime,$endTime,true);
-
-        if(Employee::where('user_id', $userLocal->id)->whereIn('college_id',$allowedColleges)->doesntExist() && $timeCheck && sizeof($allowedColleges)>0){
-            return redirect()->back()->with('error', 'The college you are in is not scheduled to login today');
-        }
-
         if($userLocal == null){
+            if($this->scheduleCheck($userLocal->id)){
+                return redirect()->back()->with('error', 'The college you are in is not scheduled to login today');
+            }
             $user = $this->db_ext->select(" EXEC GetUserAccount '$request->email' ");
 
             if($this->save($user)){
